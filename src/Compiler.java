51c69d54f35382a63654f9dd0e4cd55bb2e77477
ismail elyamany <eng-ismail.elyamany1520@alexu.edu.eg>
2020-03-29 14:16:01 +0200

    Read the file, parse the keywords

--- src/Compiler.java
+++ src/Compiler.java
@@ -3,10 +3,9 @@ import lexicalanalyzergenerator.LexicalAnalyzerGenerator;
 
 public class Compiler {
     public static void main(String[] args) {
-        LexicalAnalyzerGenerator lexicalAnalyzerGenerator = new LexicalAnalyzerGenerator("");
+        LexicalAnalyzerGenerator lexicalAnalyzerGenerator = new LexicalAnalyzerGenerator(System.getProperty("user.dir") + "\\input.txt");
         LexicalAnalyzer lexicalAnalyzer = lexicalAnalyzerGenerator.getLexicalAnalyzer();
         lexicalAnalyzer.setInputProgram("");
-
 
         lexicalAnalyzer.getNext();
     }
--- src/lexicalanalyzergenerator/LexicalAnalyzerGenerator.java
+++ src/lexicalanalyzergenerator/LexicalAnalyzerGenerator.java
@@ -1,5 +1,6 @@
 package lexicalanalyzergenerator;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
@@ -13,8 +14,11 @@ public class LexicalAnalyzerGenerator {
 
 
     public LexicalAnalyzer getLexicalAnalyzer() {
+        System.out.println(this.pathToRules);
+        File file = new File(this.pathToRules);
+
         // Build NFAs
-        Map<String, NFA> tokenNFAs = buildNFAs();
+        Map<String, NFA> tokenNFAs = buildNFAs(file);
 
         // Combine NFAs
         NFA combinedNFA = combineNFAs(tokenNFAs);
@@ -35,7 +39,34 @@ public class LexicalAnalyzerGenerator {
      * @return Map of key value pairs. The key denotes the token that this NFA matches
      * while the value denotes the NFA
      */
-    private HashMap<String, NFA> buildNFAs() {
+    private HashMap<String, NFA> buildNFAs(File file) {
+        HashMap<String, NFA> nfas = new HashMap<>();
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+            while((line = bufferedReader.readLine())!=null){
+                Stack st = new Stack();
+
+                //Keywords
+                if(line.startsWith("{")){
+                    line=line.substring(1,line.length()-1);
+
+                    String[] keywords = line.trim().split(" ");
+                    for (int i = 0; i < keywords.length; i++) {
+                        Stack s = new Stack();
+                        s.push(keywords[i]);
+                        nfas.put(keywords[i], buildNFA(s));
+                        System.out.println("Here at " + i + " " + keywords[i]);
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         Map<String, NFA> tokenNFAs = new HashMap<>();
         return null;
     }
@@ -47,6 +78,8 @@ public class LexicalAnalyzerGenerator {
      * @return NFA
      */
     private NFA buildNFA(Stack stack) {
+        System.out.println("STACK");
+        System.out.println(stack);
         return null;
     }
 

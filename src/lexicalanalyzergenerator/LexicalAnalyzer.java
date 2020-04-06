adef44007e48d6cad2dee84cb7880cf222e47e06
OmarWagdySalah <46059054+OmarWagdySalah@users.noreply.github.com>
2020-04-06 18:15:42 +0200

    Lexical Analyzer

--- src/lexicalanalyzergenerator/LexicalAnalyzer.java
+++ src/lexicalanalyzergenerator/LexicalAnalyzer.java
@@ -6,16 +6,16 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class LexicalAnalyzer {
-    private static int iterator;
+	private static int iterator;
     private DFA dfa;
-    private static char head;
-    private String program;
+	private static char head;
+	private String program;
+    
 
-
-    public LexicalAnalyzer(DFA dfa) {
+	public LexicalAnalyzer(DFA dfa) {
         this.dfa = dfa;
-        this.iterator = 0;
-        this.program = "";
+        this.iterator=0;
+        this.program="";
     }
 
 //	public static int getIterator() {
@@ -35,25 +35,25 @@ public class LexicalAnalyzer {
 //	}
 //	
 
-    public void setInputProgram(String pathToProgram) {
-        String data = "";
+    public void setInputProgram(String pathToProgram){
+    	String data = "";
+		
+	      File myObj = new File(pathToProgram);
+	      Scanner myReader;
+		try {
+			myReader = new Scanner(myObj);
+		
+	      while (myReader.hasNextLine()) {
+	         data += myReader.nextLine();
+	      }
+	      myReader.close();
 
-        File myObj = new File(pathToProgram);
-        Scanner myReader;
-        try {
-            myReader = new Scanner(myObj);
-
-            while (myReader.hasNextLine()) {
-                data += myReader.nextLine();
-            }
-            myReader.close();
-
-            System.out.println(data);
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        this.program = data;
+    System.out.println(data);
+		} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+		this.program=data;
 
     }
 
@@ -62,56 +62,78 @@ public class LexicalAnalyzer {
      *
      * @return String
      */
-    public String getNext() {
+    public String getNext(){
+       
+    	Stack<String> tokens=new Stack<>();
+		Stack<String> labels=new Stack<>();
+		
 
-        Stack<String> tokens = new Stack<>();
-        Stack<String> labels = new Stack<>();
+		String token="",label;
+		// skip first space
+		this.dfa.reset();
+		if(this.program.charAt(this.iterator)==' ') this.iterator++;
+			
+		while (this.iterator<this.program.length())
+		{      
+		
+			token+=this.program.charAt(this.iterator);
+			head=this.program.charAt(this.iterator);
+			//get the label from the DFA
+			label =this.dfa.advance(this.head);
+			
+			
+			if(label!=null)		
+			{
+				tokens.push(token);
+				labels.push(label);
+				this.iterator++;
+			}
+			else {
+				
+				if(!labels.isEmpty()) {
+					this.iterator--;
+					this.dfa.reset();
+					//print token's label
+					System.out.println(labels.peek()+"    "+tokens.peek());
+					return labels.pop();
 
-        String token = "", label;
-        // skip first space
-        if (this.program.charAt(this.iterator) == ' ') this.iterator++;
-
-        while (this.iterator < this.program.length()) {
-
-            token += this.program.charAt(this.iterator);
-            head = this.program.charAt(this.iterator);
-            //get the label from the DFA
-            label = this.dfa.advance(this.head);
-
-
-            if (label != null) {
-                tokens.push(token);
-                labels.push(label);
-                this.iterator++;
-            } else {
-
-                if (!labels.isEmpty()) {
-                    this.iterator--;
-                    this.dfa.reset();
-                    //print token's label
-                    System.out.println(labels.peek() + "    " + tokens.peek());
-                    return labels.pop();
-
-                }
-
-            }
-
-        }
-
-
-        return null;
-
+				}
+				
+			}
+			
+		}
+		
+		
+		
+		return null;
+		
+        
+        
+        
+        
     }
 
+    
+    
+    
+    
+    
     /**
      * Checks if there is a next token from
      * input program
      *
      * @return boolean
      */
-    public boolean hasNext() {
-        if (this.iterator < this.program.length())
-            return true;
+    public boolean hasNext(){
+        if(this.iterator<this.program.length())
+        		return true;
         return false;
     }
+    public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+		//Read test program from 
+    	LexicalAnalyzer x=new LexicalAnalyzer(null);
+		x.setInputProgram(System.getProperty("user.dir") + "/src" + "//TestProgram.txt");
+		x.getNext();
+	}
 }

dce43ff0ea6bbb673c4252da7e83f095628d40e4
OmarWagdySalah <46059054+OmarWagdySalah@users.noreply.github.com>
2020-05-05 23:59:32 +0200

    final commit

--- src/Compiler.java
+++ src/Compiler.java
@@ -4,7 +4,7 @@ import parsergenerator.ParserGenerator;
 
 public class Compiler {
     public static void main(String[] args) {
-        LexicalAnalyzerGenerator lexicalAnalyzerGenerator = new LexicalAnalyzerGenerator(System.getProperty("user.dir") + "/src" + "//input-rules-3.txt");
+        LexicalAnalyzerGenerator lexicalAnalyzerGenerator = new LexicalAnalyzerGenerator(System.getProperty("user.dir") + "/src" + "//input-rules-4.txt");
         ParserGenerator parserGenerator = new ParserGenerator(System.getProperty("user.dir") + "/src" + "//CFG-input.txt");
 
 
